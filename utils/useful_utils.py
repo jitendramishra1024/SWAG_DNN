5ba039788824b564a40c713a444fbee5ddf7c2d6
jitendramishra1024 <jitendramishra1024@gmail.com>
2021-03-14 23:14:11 +0530

    Update useful_utils.py

--- utils/useful_utils.py
+++ utils/useful_utils.py
@@ -2,10 +2,10 @@ def modify_train_loss_accuracy_from_batch_level_to_epoch_level(train_acc,train_l
   new_train_acc=[]
   sum=0
   count=0
-  for i in range (len(train_acc))
+  for i in range (len(train_acc)):
     sum=sum+train_acc[i]
     count=count+1
-    if count%len(trainloader)==0
+    if count%len(trainloader)==0:
       new_train_acc.append(sumlen(trainloader))
       sum=0
 
@@ -13,10 +13,10 @@ def modify_train_loss_accuracy_from_batch_level_to_epoch_level(train_acc,train_l
   new_train_loss=[]
   sum=0
   count=0
-  for i in range (len(train_losses))
+  for i in range (len(train_losses)):
     sum=sum+train_losses[i]
     count=count+1
-    if count%len(trainloader)==0
+    if count%len(trainloader)==0:
       new_train_loss.append(sumlen(trainloader))
       sum=0
   return new_train_acc,new_train_loss
