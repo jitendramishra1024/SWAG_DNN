0423c5370998bf15e33a2b16a7202601cdcb92e3
jitendramishra1024 <jitendramishra1024@gmail.com>
2021-03-19 12:30:24 +0530

    Update RESNET.py

--- models/RESNET.py
+++ models/RESNET.py
@@ -61,5 +61,22 @@ class ResNet(nn.Module):
         return out
 
 
+    
 def ResNet18():
     return ResNet(BasicBlock, [2, 2, 2, 2])
+
+
+def ResNet34():
+    return ResNet(BasicBlock, [3, 4, 6, 3])
+
+
+def ResNet50():
+    return ResNet(Bottleneck, [3, 4, 6, 3])
+
+
+def ResNet101():
+    return ResNet(Bottleneck, [3, 4, 23, 3])
+
+
+def ResNet152():
+    return ResNet(Bottleneck, [3, 8, 36, 3])
